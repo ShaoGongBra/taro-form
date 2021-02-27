88a9b36be83c9856219d0aedc9a1c8848b2424ee
ShaoGongBra <31879769+ShaoGongBra@users.noreply.github.com>
2021-02-27 17:53:02 +0800

    完善弹出选择

--- src/main/form/common/select.jsx
+++ src/main/form/common/select.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useMemo, useCallback, useState, useRef } from 'react'
 import Taro from '@tarojs/taro'
 import { View, Text, Image, ScrollView } from '@tarojs/components'
 import Icon from '../../../components/icon'
@@ -9,15 +9,96 @@ import Base from './base'
 import { getContrastYIQ } from '../../../utils/color'
 import './select.scss'
 
-export default class SelectForm extends Component {
-
-  input(item, isSelect) {
-    let { data = {}, config = {}, value } = this.props
-    if (config.edit || isSelect) {
-      return
+// 判断当前项目是否选中
+const isSelect = (item, type, value = '') => {
+  if (type === 'radio') {
+    if (typeof item.value === 'object') {
+      return item.value[0] === value
     }
+    return item.value === value
+  } else {
+    return (typeof value === 'string' ? value.split(',') : value).includes(item.value)
+  }
+}
+
+const RenderItemIcon = ({ item, type = 'radio', value }) => {
+
+  const icon = type === 'radio'
+    ? (isSelect(item, type, value) ? 'fuhao-zhuangtai-chenggong' : 'danxuan-weixuan')
+    : (isSelect(item, type, value) ? 'duoxuan-yixuan' : 'duoxuan-weixuan')
+
+  return <Icon name={icon} size={42} style={{ marginRight: Taro.pxTransform(12) }} />
+}
+
+const RenderText = ({ item, onInput, type, value }) => {
+  return <View className='form-select__item-text' onClick={() => onInput(item)}>
+    <RenderItemIcon item={item} type={type} value={value} />
+    <View className='form-select__item-text__right'>
+      <Text className='form-select__item-text__name'>{item.text}</Text>
+      {!!item.desc && <Text className='form-select__item-text__desc'>{item.desc}</Text>}
+    </View>
+  </View>
+}
+
+const RenderButton = ({ item, onInput, type, value }) => {
+  return <Btn
+    text={item.text}
+    size='m'
+    disabled={!!item.disabled}
+    plain={!isSelect(item, type, value)}
+    beforeImage={item.image}
+    style={{
+      marginRight: Taro.pxTransform(10),
+      marginBottom: Taro.pxTransform(10)
+    }}
+    onClick={() => onInput(item)}
+  />
+}
+
+const RenderCard = ({ item, onInput, type, value }) => {
+  return <View className='form-select__item-card' onClick={() => onInput(item)}>
+    <RenderItemIcon item={item} type={type} value={value} />
+    {item.image && <Image className='form-select__item-card__image' src={item.image} />}
+    <View className='form-select__item-card__right'>
+      <Text className='form-select__item-card__name'>{item.text}</Text>
+      {!!item.desc && <Text className='form-select__item-card__desc'>{item.desc}</Text>}
+    </View>
+  </View>
+}
+
+const RenderImage = ({ item, onInput, type, value }) => {
+
+  return <View className='form-select__item-image' onClick={() => onInput(item)}>
+    <View className='form-select__item-image__btn'>
+      <RenderItemIcon item={item} type={type} value={value} />
+    </View>
+    <Image className='form-select__item-image__image' src={item.image} />
+    {!!item.text && <View className='form-select__item-image__bottom'>
+      <Text className='form-select__item-image__name'>{item.text}</Text>
+      {!!item.desc && <Text className='form-select__item-image__desc'>{item.desc}</Text>}
+    </View>}
+  </View>
+}
+
+const RenderColor = ({ item, onInput, type, value }) => {
+  return <View
+    className='form-select__item-color'
+    style={{ backgroundColor: item.color }}
+    onClick={() => onInput(item)}
+  >
+    {isSelect(item, type, value) && <Icon name='duihao2' color={getContrastYIQ(item.color)} />}
+  </View>
+}
+
+const RenderOption = ({ onlySelect, data = {}, value, onInput }) => {
+  if (!data.option) {
+    data.option = []
+  }
+
+  const list = onlySelect ? data.option.filter(item => isSelect(item, data.type, value)) : data.option
+  const input = (item) => {
     if (data.type === 'radio') {
-      this.props.onEvent({
+      onInput({
         event: 'input',
         component: data,
         value: item.value
@@ -27,144 +108,105 @@ export default class SelectForm extends Component {
       if (valueType === 'string') {
         value = value.split(',')
       }
+      value = [...value]
       if (value.includes(item.value)) {
         value.splice(value.indexOf(item.value), 1)
       } else {
         value.push(item.value)
       }
-      this.props.onEvent({
+      onInput({
         event: 'input',
         component: data,
         value: valueType === 'string' ? value.join(',') : value
       })
     }
-    // 弹出单选 选择关闭选框
-    if (data.type === 'radio' && data.mode === 'picker') {
-      this.pullView.close()
-    }
   }
 
-  // 判断当前项目是否选中
-  isSelect(item) {
-    const { data = {}, value = '' } = this.props
-    if (data.type === 'radio') {
-      if (typeof item.value === 'object') {
-        return item.value[0] === value
-      }
-      return item.value === value
-    } else {
-      return (typeof value === 'string' ? value.split(',') : value).includes(item.value)
+  return <View className={`form-select form-select--row form-select--${data.theme}`}>
+    {
+      list.map(item => {
+        const itemData = { item, type: data.type, value, onInput: input, key: item.value }
+        {
+          switch (data.theme) {
+            case 'text':
+              return <RenderText {...itemData} />
+            case 'button':
+              return <RenderButton {...itemData} />
+            case 'card':
+              return <RenderCard {...itemData} />
+            case 'image':
+              return <RenderImage {...itemData} />
+            case 'color':
+              return <RenderColor {...itemData} />
+            default:
+              break
+          }
+        }
+      })
     }
+  </View>
+}
+
+const Picker = ({ data, onInput, onClose, value }) => {
+
+  const pullView = useRef(null)
+  const [item, setItem] = useState(null)
+  // 弹出框value不更新 使用本地value
+  const [localValue, setLocalValue] = useState(value)
+
+  const submit = () => {
+    pullView.current.close()
+    item && onInput(item)
+  }
+  return <PullView
+    ref={pullView}
+    style={{ backgroundColor: '#fff', padding: Taro.pxTransform(20) }}
+    onClose={onClose}
+  >
+    <View className='form-select__pull__head'>
+      <Text className='form-select__pull__head__title'>选择</Text>
+    </View>
+    <ScrollView scrollY className='form-select__pull__scroll'>
+      <RenderOption
+        data={data}
+        value={localValue}
+        onInput={e => {
+          if (data.type === 'radio') {
+            pullView.current.close()
+            onInput(e)
+          } else {
+            setItem(e)
+            setLocalValue(e.value)
+          }
+
+        }}
+      />
+    </ScrollView>
+    {data.type === 'checkbox' && <Btn text='确定' onClick={submit} />}
+  </PullView>
+}
+
+export default class SelectForm extends Component {
+
+  input(onlySelect, item) {
+    const { config = {} } = this.props
+    if (config.edit || onlySelect) {
+      return
+    }
+    this.props.onEvent(item)
   }
 
   /**
    * 弹窗选择
    */
   picker() {
-    const { data = {} } = this.props
-    this.key = TopView.add(<PullView
-      ref={ref => this.pullView = ref}
-      style={{ backgroundColor: '#fff', padding: Taro.pxTransform(20) }}
-      onClose={() => TopView.remove(this.key)}
-    >
-      <View className='form-select__pull__head'>
-        <Text className='form-select__pull__head__title'>选择</Text>
-      </View>
-      <ScrollView scrollY className='form-select__pull__scroll'>
-        {this.renderOption()}
-      </ScrollView>
-      {data.type === 'checkbox' && <Btn text='确定' onClick={() => this.pullView.close()} />}
-    </PullView>)
-  }
-
-  renderItemIcon(item) {
-    const { data = {} } = this.props
-    const icon = data.type === 'radio'
-      ? (this.isSelect(item) ? 'fuhao-zhuangtai-chenggong' : 'danxuan-weixuan')
-      : (this.isSelect(item) ? 'duoxuan-yixuan' : 'duoxuan-weixuan')
-    return <Icon name={icon} size={42} style={{ marginRight: Taro.pxTransform(12) }} />
-  }
-
-  renderText(item, isSelect) {
-    return <View className='form-select__item-text' onClick={this.input.bind(this, item, isSelect)}>
-      {this.renderItemIcon(item)}
-      <View className='form-select__item-text__right'>
-        <Text className='form-select__item-text__name'>{item.text}</Text>
-        {!!item.desc && <Text className='form-select__item-text__desc'>{item.desc}</Text>}
-      </View>
-    </View>
-  }
-
-  renderButton(item, isSelect) {
-    return <Btn
-      text={item.text}
-      size='m'
-      disabled={!!item.disabled}
-      plain={!this.isSelect(item)}
-      beforeImage={item.image}
-      style={{
-        marginRight: Taro.pxTransform(10),
-        marginBottom: Taro.pxTransform(10)
-      }}
-      onClick={this.input.bind(this, item, isSelect)}
-    />
-  }
-
-  renderCard(item, isSelect) {
-    return <View className='form-select__item-card' onClick={this.input.bind(this, item, isSelect)}>
-      {this.renderItemIcon(item)}
-      {item.image && <Image className='form-select__item-card__image' src={item.image} />}
-      <View className='form-select__item-card__right'>
-        <Text className='form-select__item-card__name'>{item.text}</Text>
-        {!!item.desc && <Text className='form-select__item-card__desc'>{item.desc}</Text>}
-      </View>
-    </View>
-  }
-
-  renderImage(item, isSelect) {
-    return <View className='form-select__item-image' onClick={this.input.bind(this, item, isSelect)}>
-      <View className='form-select__item-image__btn'>{this.renderItemIcon(item)}</View>
-      <Image className='form-select__item-image__image' src={item.image} />
-      {!!item.text && <View className='form-select__item-image__bottom'>
-        <Text className='form-select__item-image__name'>{item.text}</Text>
-        {!!item.desc && <Text className='form-select__item-image__desc'>{item.desc}</Text>}
-      </View>}
-    </View>
-  }
-
-  renderColor(item, isSelect) {
-    return <View
-      className='form-select__item-color'
-      style={{ backgroundColor: item.color }}
-      onClick={this.input.bind(this, item, isSelect)}
-    >
-      {this.isSelect(item) && <Icon name='duihao2' color={getContrastYIQ(item.color)} />}
-    </View>
-  }
-
-  /**
-   *
-   * @param {boolean} isSelect 是否仅渲染选中项
-   */
-  renderOption(isSelect = false) {
-    const { data = { option: [] } } = this.props
-    const list = isSelect ? data.option.filter(item => this.isSelect(item)) : data.option
-    return <View className={`form-select form-select--row form-select--${data.theme}`}>
-      {
-        list.map(item => <View key={item.value}>
-          {data.theme === 'text' && this.renderText(item, isSelect)}
-          {data.theme === 'button' && this.renderButton(item, isSelect)}
-          {data.theme === 'card' && this.renderCard(item, isSelect)}
-          {data.theme === 'image' && this.renderImage(item, isSelect)}
-          {data.theme === 'color' && this.renderColor(item, isSelect)}
-        </View>)
-      }
-    </View>
+    const { data = {}, value } = this.props
+    this.key = TopView.add(<Picker data={data} value={value} onInput={this.input.bind(this, false)} onClose={() => TopView.remove(this.key)} />)
   }
 
   render() {
-    const { data = { option: [] }, config = {} } = this.props
-    const isEmpty = data.option.filter(item => this.isSelect(item)).length === 0
+    const { data = { option: [] }, config = {}, value } = this.props
+    const isEmpty = data.mode === 'picker' && data.option.filter(item => isSelect(item, data.type, value)).length === 0
     return <Base {...this.props}>
       {
         data.mode === 'picker' ?
@@ -177,10 +219,10 @@ export default class SelectForm extends Component {
               this.picker()
             }}
           >
-            {this.renderOption(true)}
+            <RenderOption {...this.props} onlySelect onInput={this.input.bind(this, true)} />
             {isEmpty && <Text className='form-select__value__select'>点击选择</Text>}
           </View> :
-          this.renderOption()
+          <RenderOption {...this.props} onInput={this.input.bind(this, false)} />
       }
     </Base>
   }
